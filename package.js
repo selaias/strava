68e64a51daba398bc126959b15e3edf153f5ba7a
selaias <s.elaias@gmail.com>
2015-06-08 08:47:47 +0000

    removed force from approval_prompt

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'selaias:strava',
-  version: '0.1.1',
+  version: '0.2.0',
   summary: 'An implementation of the Strava OAuth flow.',
   git: 'https://github.com/selaias/strava.git',
   documentation: 'README.md'
