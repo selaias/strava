0d3d4aa12cfc42585dfd38b1ac5729baacc0c149
selaias <s.elaias@gmail.com>
2015-10-24 17:03:10 +0000

    added scope support

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'selaias:strava',
-  version: '0.2.0',
+  version: '0.3.0',
   summary: 'An implementation of the Strava OAuth flow.',
   git: 'https://github.com/selaias/strava.git',
   documentation: 'README.md'
